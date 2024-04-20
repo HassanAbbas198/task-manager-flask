ca28b55824aa4c49b992d416656fe242a1c75f10
Hassan Abbas <hassan.abbasCS@gmail.com>
2024-04-20 20:11:13 +0300

    refactor: use dictionary unpacking for task creation

--- app/task/repository.py
+++ app/task/repository.py
@@ -3,10 +3,7 @@ from .model import Task
 
 class TaskRepository:
     def create_task(self, data):
-        title = data.get('title')
-        description = data.get('description')
-
-        task = Task(title=title, description=description)
+        task = Task(**data)
         db.session.add(task)
         db.session.commit()
         return task
