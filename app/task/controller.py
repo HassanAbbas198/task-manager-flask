ed9751a6b337e22f4cfd848825c96a538ca773e1
Hassan Abbas <hassan.abbasCS@gmail.com>
2024-04-20 23:41:17 +0300

    feat: integrate marshmallow for request validation

--- app/task/controller.py
+++ app/task/controller.py
@@ -1,18 +1,28 @@
 from flask import Blueprint, jsonify, request
 from app.error_handlers import error_formatter
 from .service import TaskService
-from .exceptions import TaskNotFoundError
+from .exceptions import TaskNotFoundError, ValidationError
+from .schemas import CreateTaskSchema, UpdateTaskSchema
 
 task_bp = Blueprint('task', __name__)
 task_service = TaskService()
+create_task_schema = CreateTaskSchema()
+update_task_schema = UpdateTaskSchema()
 
 
 @task_bp.route('/', methods=['POST'])
 def create_task():
-    data = request.json
-    response = task_service.create_task(data)
+    try:
+        data = request.json
 
-    return jsonify({'data': response}), 201
+        errors = create_task_schema.validate(data)
+        if errors:
+            raise ValidationError(message=str(errors))
+
+        response = task_service.create_task(data)
+        return jsonify({'data': response}), 201
+    except ValidationError as e:
+        return error_formatter(400, str(e))
 
 
 @task_bp.route('/', methods=['GET'])
@@ -36,8 +46,14 @@ def update_task(task_id):
     try:
         data = request.json
 
+        errors = update_task_schema.validate(data)
+        if errors:
+            raise ValidationError(message=str(errors))
+
         response = task_service.update_task(task_id, data)
         return jsonify({'data': response})
+    except ValidationError as e:
+        return error_formatter(400, str(e))
     except TaskNotFoundError as e:
         return error_formatter(404, str(e))
 
