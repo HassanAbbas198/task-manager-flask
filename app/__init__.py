9202646b4f3c20060c0c7af975ead85580327e36
Hassan Abbas <hassan.abbasCS@gmail.com>
2024-04-20 12:28:43 +0300

    feat: create a task_blueprint and register it with Flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
+from app.task.controllers import task_bp
 from config import Config
 
 db = SQLAlchemy()
@@ -9,6 +10,11 @@ def create_app():
 
     # Load configuration
     app.config.from_object(Config)
+
+    # Create a parent blueprint and register it with its sub-blueprints with the Flask app
+    parent_bp = Blueprint('parent', __name__, url_prefix='/api/v1')
+    parent_bp.register_blueprint(task_bp, url_prefix='/tasks')
+    app.register_blueprint(parent_bp)
 
     # Initialize Flask-SQLAlchemy
     db.init_app(app)
--- config.py
+++ config.py
@@ -9,6 +9,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     DEBUG = os.environ.get('DEBUG') or True
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
-        or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
